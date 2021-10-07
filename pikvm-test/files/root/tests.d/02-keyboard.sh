26c7d6bc4f80b8269419222826ba3a93e1b8fa96
Vladimir Sukhonosov <founder.sl@gmail.com>
2021-10-07 17:29:10 +0300

    Revert "test stage (#19)" (#23)
    This reverts commit 9ed02d9908c11d41f6b42465b3788cab46a1b625.

--- pikvm-test/files/root/tests.d/02-keyboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TITLE="Keyboard"
-TESTSTRING="This is the KEYBOARD test."
-
-TEXT=`Xdialog --title $TITLE --inputbox "Please type the following text case sensitive:\n\n$TESTSTRING" 0x0 2>&1`
-RET=$?
-[ $? -ne 0 ] && exit $RET
-[ x"$TEXT" = x"$TESTSTRING" ] || exit 2
